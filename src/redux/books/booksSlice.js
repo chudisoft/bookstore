9c10da3c870365dc4f92e0a6db6bbbbc23b35185
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-08-22 19:54:15 +0100

    Implement Bookstore API for create and delete actions

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,54 +1,101 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+const baseApiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/s4Jy4vQC08zpfLQKgC81/';
+
+// First, create the thunk
+const fetchBooks = createAsyncThunk(
+  'books/fetchBooks',
+  async () => {
+    const response = await axios.get(`${baseApiUrl}books`);
+    return response.data;
+  },
+);
+
+const addBook = createAsyncThunk(
+  'books/addBook',
+  async (book) => {
+    const response = await axios.post(`${baseApiUrl}books`, book);
+    return response.data === 'Created' ? book : null;
+  },
+);
+
+const removeBook = createAsyncThunk(
+  'books/removeBook',
+  async (book) => {
+    const response = await axios.delete(`${baseApiUrl}books/${book.item_id}`);
+    return response.data === 'The book was deleted successfully!' ? book : null;
+  },
+);
 
 const initialState = {
-  value: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-      percentage: 64,
-      chapter: 'Chapter 17',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-      percentage: 8,
-      chapter: 'Chapter 3: "A Lesson Learned"',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-      percentage: 0,
-      chapter: 'Introduction',
-    },
-  ],
+  books: [],
+  error: '',
+  loading: 'idle', // 'idle' | 'pending' | 'succeeded' | 'failed'
 };
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value.push(action.payload);
-    },
-    removeBook: (state, action) => {
-      state.value.pop(initialState.value.find((x) => x.item_id === action.payload.item_id));
-      let i = 1;
-      state.value.forEach((x) => { x.item_id = `item ${(i += 1)}`; });
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(fetchBooks.pending, (state) => {
+      state.status = 'loading';
+    }).addCase(fetchBooks.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      if (action.payload !== '') {
+        const books = [];
+        const keys = Object.keys(action.payload);
+        keys.forEach((x) => {
+          books.push({ item_id: x, ...action.payload[x][0] });
+        });
+        state.books = books;
+        if (state.books.length === 0) state.error = 'No result was found!';
+      } else {
+        state.error = 'No result was found!';
+      }
+    }).addCase(fetchBooks.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
+    builder.addCase(addBook.pending, (state) => {
+      state.status = 'loading';
+    }).addCase(addBook.fulfilled, (state, action) => {
+      if (action.payload !== null) {
+        state.status = 'succeeded';
+        state.error = '';
+        state.books.push(action.payload);
+      } else {
+        state.status = 'failed';
+        state.error = 'Unable to add record!';
+      }
+    }).addCase(addBook.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
+    builder.addCase(removeBook.pending, (state) => {
+      state.status = 'loading';
+    }).addCase(removeBook.fulfilled, (state, action) => {
+      if (action.payload !== null) {
+        state.status = 'succeeded';
+        state.error = '';
+        state.books = state.books.filter((x) => x.item_id !== action.payload.item_id);
+        if (state.books.length === 0) state.error = 'No result was found!';
+      } else {
+        state.status = 'failed';
+        state.error = 'Unable to remove record!';
+      }
+    }).addCase(removeBook.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addBook, removeBook } = booksSlice.actions;
+// export const { addBook, removeBook } = booksSlice.actions;
+export { addBook, fetchBooks, removeBook };
 
 export default booksSlice.reducer;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,7 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  categories: [],
+  categories: [
+    'Action',
+    'Science Fiction',
+    'Economy',
+    'Music',
+  ],
 };
 
 export const categoriesSlice = createSlice({
@@ -9,10 +14,6 @@ export const categoriesSlice = createSlice({
   initialState,
   reducers: {
     addCategory: (state, action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.categories.push(action.payload);
     },
     removeCategory: (state, action) => {
