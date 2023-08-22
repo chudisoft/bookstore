fa47632f01e7e79d0406de29f2045f114aa35753
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-08-22 09:19:31 +0100

    Use redux store

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import { checkStatus } from "../../redux/categories/categoriesSlice";
 
 function Categories() {
-  const status = useSelector((state) => state.categories.status);
-  // const dispatch = useDispatch();
-
+  const dispatch = useDispatch();
+  dispatch(checkStatus());
+  const categories = useSelector((state) => state.categories.categories);
+  
   return (
     <div>
       <h2>Categories</h2>
-      <div>{status}</div>
+      <div>
+        {categories.map((c) => (
+          <li>{c}</li>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-Categories.propTypes = {}
+Categories.propTypes = {};
 
-export default Categories
+export default Categories;
